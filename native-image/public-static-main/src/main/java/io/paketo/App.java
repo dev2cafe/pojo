05acf406c5022aebcb6047f0ffc3ad8888a72384
dev2cafe <dev2cafe>
2022-03-08 02:43:17 -0800

    shade

--- native-image/public-static-main/src/main/java/io/paketo/App.java
+++ native-image/public-static-main/src/main/java/io/paketo/App.java
@@ -1,9 +1,13 @@
 package io.paketo;
 
+import io.paketo.CosExample;
+
 public class App 
 {
     public static void main( String[] args )
     {
+	CosExample cos = new CosExample();
         System.out.println(System.getenv("STR_KEY") +  "Hello!" );
+	System.out.println(cos.test());
     }
 }
